eb589ac06caa26f757d1444431306b7d215d6d80
Weisenreder <12576@Telos.Corp>
2017-07-06 12:37:37 -0400

    New Bat

--- testscript.bat
+++ testscript.bat
@@ -1,1 +1,4 @@
-echo "Hello World"
+Echo Off                              
+Echo Hello World. Here is my first line of shell scripting code!
+Echo Hello World. Here is my second line of shell scripting code!                              
+Echo Hello World. Here is my third line of shell scripting code!
